6624b6a610b0fd760a67b7e0fe2f343fedea4cae
Lucio Dery <ldery@compute-0-31.local>
2020-11-06 02:36:04 -0500

    initial implementation of learning algo - now to work on main

---
+++ algorithms/algos.py
@@ -0,0 +1,176 @@
+#     Copyright 2020 Google LLC
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#         https://www.apache.org/licenses/LICENSE-2.0
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+import torch
+import numpy as np
+from torch.optim import Adam, AdamW
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from tqdm import tqdm
+from collections import defaultdict, namedtuple
+from os import path
+import pickle
+import pdb
+from sklearn import metrics
+from .utils import *
+
+
+
+def add_trainer_args(parser):
+	parser.add_argument('-train-epochs', type=int, default=50)
+	parser.add_argument('-patience', type=int, default=15)
+	parser.add_argument('-lr-patience', type=int, default=4)
+	parser.add_argument('-optimizer', type=str, default='Adam')
+	parser.add_argument('-lr', type=float, default=3e-4)
+	parser.add_argument('-batch-sz', type=int, default=128)
+	parser.add_argument('-chkpt-path', type=str, default='experiments')
+	parser.add_argument('-use-last-chkpt', action='store_true', help='Instead of using best, use last checkpoint')
+	parser.add_argument('-continue-from-last', action='store_true')
+
+
+class Trainer(object):
+	def __init__(
+					self, train_epochs, chkpt_path, patience,
+				):
+		self.chkpt_every = 10  # save to checkpoint
+		self.train_epochs = train_epochs
+		self.chkpt_path = chkpt_path
+		self.patience = patience
+		self.max_grad_norm = 1.0
+
+	def get_optim(self, model, opts, ft=False):
+		lr = opts.lr if not ft else opts.finetune_lr
+		if opts.optimizer == 'Adam':
+			optim = Adam(model.parameters(), lr=lr)
+		elif opts.optimizer == 'AdamW':
+			print('Using AdamW Optimizer')
+			optim = AdamW(model.parameters(), lr=lr, weight_decay=0.1)
+		else:
+			raise ValueError
+		# Reduce lr by 0.5 on plateau
+		lr_scheduler = ReduceLROnPlateau(optim, factor=0.5, patience=opts.lr_patience, min_lr=1e-5)
+		return optim, lr_scheduler
+
+	def _get_numcorrect(self, output, targets, mask=None, chxnet_src=False):
+		with torch.no_grad():
+			argmax = output.argmax(dim=-1).squeeze()
+			if output.shape[-1] == 1:  # We have only 1 output - so sigmoid.
+				argmax = (torch.nn.Sigmoid()(output) > 0.5).squeeze().float()
+			return argmax.eq(targets).sum()
+
+	def run_model(self, x, y, model, group=None, reduct_='none'):
+		model_out = model(x, head_name=group)
+		loss_fn = model.get_loss_fn(model.loss_fn_name, reduction=reduct_)
+		loss = loss_fn(model_out, y)
+		num_correct = self._get_numcorrect(model_out, y)
+		return loss, num_correct
+
+	def run_epoch(self, model, data_iter, optim):
+		model.train()
+		if optim is None:
+			model.eval()
+		stats = defaultdict(lambda: [0.0, 0.0, 0.0])
+		for batch in data_iter:
+			if optim is not None:
+				optim.zero_grad()
+			for key, (xs, ys) in batch:
+				results = self.run_model(xs, ys, model, group=key, reduct_='mean')
+				stats[key][0] += results[0].item() * len(ys)
+				stats[key][1] += results[1].item()
+				stats[key][2] += len(ys)
+				results[0].backward()  # Accumulate the gradient so you don't hold on to graph
+
+			if optim is not None:
+				loss.backward()
+				# torch.nn.utils.clip_grad_norm_(model.parameters(), self.max_grad_norm)
+				optim.step()
+		summary = {}
+		for k, v in stats.items():
+			summary[k] = (v[0] / v[2], v[1] / v[2])
+		return summary
+
+	def model_exists(self, model, dataset, kwargs):
+		chkpt_path = kwargs["model_chkpt_path"]
+		dir_name = path.dirname(chkpt_path)
+		metric_path = path.join(dir_name, "train_metrics.pkl")
+		if not path.exists(metric_path):
+			return None, None
+		train_metrics = pickle.load(open(metric_path, 'rb'))
+		best_val_loss = min(train_metrics['monitor_loss'][-(self.patience + 1):])
+		final_path = self.get_chkpt_path('best', chkpt_path)
+		if path.exists(final_path):
+			model.load_state_dict(torch.load(final_path))
+			return train_metrics, best_val_loss
+		else:
+			return None, None
+
+	def get_chkpt_path(self, epoch, chkpt_path):
+		this_path = chkpt_path.split('.')[:-1]
+		this_path = ".".join(this_path)
+		return this_path + "_epoch_" + str(epoch) + ".chkpt"
+
+	def sanity_check_args(self, arg_dict):
+		assert 'classes' in arg_dict, 'List of classes not in arguments'
+		assert 'batch_sz' in arg_dict, 'Arg dict must have batch size specified'
+		assert 'model_chkpt_fldr' in arg_dict, 'Need to specify where to checkpoint model'
+		assert 'monitor_list' in arg_dict, 'Need to specify which super-classes are used for val monitoring'
+
+	def train(self, model, dataset, optim, lr_scheduler=None, **kwargs):
+		# Do the training
+		# Check if the model already exists and just return it
+		self.sanity_check_args(kwargs)
+		self.metrics = defaultdict(list)
+		best_val_loss = float('inf')
+		saved_info = self.model_exists(model, dataset, kwargs)
+		if saved_info[0] is not None:
+			return saved_info[0], saved_info[1]
+		iterator = tqdm(range(self.train_epochs))
+		chkpt_path = kwargs["model_chkpt_fldr"]
+		best_path = os.path.join(chkpt_path, 'best.chkpt')
+		last_path = os.path.join(chkpt_path, 'last.chkpt')
+		monitor_list = kwargs['monitor_list']
+		monitor_metric = []
+		for i in iterator:
+			tr_iter = dataset._get_iterator(kwargs['classes'], kwargs['batch_sz'], split='train', shuffle=True)
+			tr_results = self.run_epoch(model, tr_iter, optim)
+			for k, v in tr_results:
+				self.metrics["tr_{}".format(k)].append(v)
+
+			val_iter = dataset._get_iterator(kwargs['classes'], kwargs['batch_sz'], split='val', shuffle=False)
+			val_results = self.run_epoch(model, val_iter, optim)
+			to_avg = []
+			for k, v in val_results:
+				self.metrics["val_{}".format(k)].append(v)
+				if k in monitor_list:
+					to_avg.append(v[0])
+			monitor_metric.append(np.mean(to_avg))
+			if lr_scheduler is not None:
+				lr_scheduler.step(monitor_metric[-1])
+			
+			if monitor_metric[-1] <= best_val_loss:
+				# We have seen an improvement in validation loss
+				best_val_loss = monitor_metric[-1]
+				torch.save(model.state_dict(), best_path)
+			else:
+				torch.save(model.state_dict(), last_path)
+
+			no_improvement = max(monitor_metric) not in monitor_metric[-self.patience:]
+			if i > self.patience and no_improvement:
+				break
+		# Load the best path
+		model.load_state_dict(torch.load(best_path))
+		test_iter = dataset._get_iterator(kwargs['classes'], kwargs['batch_sz'], split='test', shuffle=False)
+		test_results = self.run_epoch(model, test_iter, optim)
+		for k, v in test_results:
+			self.metrics["test_{}".format(k)].append(v)
+		return self.metrics, model
+
+
+
