bc945ac7639e83f655773ea9817a42760eb33cb1
Lucio Dery <ldery@compute-0-26.local>
2021-03-02 11:26:52 -0500

    added plots for test.ipynb to show to advisors

--- multi_launch_meta.sh
+++ multi_launch_meta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --job-name=varyBatchSz
-#SBATCH -e /home/ldery/meta4multitask_all/meta4multitask/m4m_cache/slurm_logs/meta/varyBatchSz.err
-#SBATCH -o /home/ldery/meta4multitask_all/meta4multitask/m4m_cache/slurm_logs/meta/varyBatchSz.out
+#SBATCH --job-name=varyAlphaReg
+#SBATCH -e /home/ldery/meta4multitask_all/meta4multitask/m4m_cache/slurm_logs/meta/varyAlphaReg.err
+#SBATCH -o /home/ldery/meta4multitask_all/meta4multitask/m4m_cache/slurm_logs/meta/varyAlphaReg.out
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=72:00:00
@@ -13,9 +13,9 @@
 
 
 # Adam. Vary Met-Head Regularization
-./run4.sh softmax 1 8 8 4 1e-3 Adam metaSmallData 1
-./run4.sh softmax 1 8 8 4 1e-3 Adam metaSmallData 100
-./run4.sh softmax 1 8 8 4 1e-3 Adam metaSmallData 500
+./run4.sh softmax 1 16 16 8 1e-3 Adam metaSmallData 1
+./run4.sh softmax 1 16 16 8 1e-3 Adam metaSmallData 100
+./run4.sh softmax 1 16 16 8 1e-3 Adam metaSmallData 500
 
 # # SGD
 # ./run4.sh linear  0 3e-2 Adam selfsupervised/SGD.3e-2
